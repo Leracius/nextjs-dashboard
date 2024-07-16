f21a346971baf75b5dc5831b6c78be3264a5d9df
Axel Quintana <lucasaxelqg15@gmail@email.com>
2024-07-16 03:57:24 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { inter } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <footer className="bg-blue-500 py-4 text-center text-white">
+          <p className="text-l">Hecho por Axel Quintana 🧑‍💻</p>
+        </footer>
+      </body>
     </html>
   );
 }
